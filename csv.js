3930827218b8524691953e0d5f0e67db95f86990
Arin <72822356+koreaneggroll@users.noreply.github.com>
2021-03-11 10:57:17 +0200

    serves html

--- csv.js
+++ csv.js
@@ -3,7 +3,12 @@ const http = require('http');
 const host = "localhost";
 const port = 8000;
 
-const requestListener = function(req, res) {};
+const requestListener = function(req, res) {
+    res.setHeader("Content-type", "text/csv");
+    res.setHeader("Content-Disposition", "attachement:filename=oceanpals.csv");
+    res.writeHead(200);
+    res.end(`id,name,email\n1, Arin Grigoras, aringrigoras@gmail.com`);
+};
 
 const server = http.createServer(requestListener);
 server.listen(port, host, () => {
